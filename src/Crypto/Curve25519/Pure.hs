fc73603c115df0a61f51d21645173ed4bedb7026
Tim Lundqvist <timluq@gmail.com>
2015-08-16 02:27:44 +0200

    Fixed typo in generatePrivate + import/export priv
    There was a typo so b31'' did not use b31' as intended but instead used the unchanged b31.
    
    I also added export and import functions for PrivateKey which otherwise would need unsafeCoerce in code using this library.

--- src/Crypto/Curve25519/Pure.hs
+++ src/Crypto/Curve25519/Pure.hs
@@ -7,6 +7,7 @@ module Crypto.Curve25519.Pure(
          PrivateKey
        , PublicKey
        , importPublic, exportPublic
+       , importPrivate, exportPrivate
        , generatePrivate
        , generatePublic
        , generateKeyPair
@@ -24,6 +25,8 @@ import Foreign.C.Types
 import Foreign.Marshal.Alloc
 import Foreign.Ptr
 import System.IO.Unsafe
+import Data.Maybe (fromJust)
+import Data.List (foldl')
 
 -- |The type of a Curve25519 private key.
 newtype PrivateKey = Priv ByteString
@@ -42,26 +45,41 @@ generatePrivate :: CryptoRandomGen g => g -> Either GenError (PrivateKey, g)
 generatePrivate g =
   case genBytes 32 g of
     Left e              -> Left e
-    Right (bytesbs, g') ->
-      let Just (b0, b1_31)  = BS.uncons bytesbs
-          Just (b1_30, b31) = BS.unsnoc b1_31
-          b0'               = b0  .&. 248
-          b31'              = b31 .&. 127
-          b31''             = b31 .|. 64
-          bytes             = (b0' `BS.cons` b1_30) `BS.snoc` b31''
-      in Right (Priv bytes, g')
+    Right (bytesbs, g') -> Right (fromJust (importPrivate bytesbs), g')
+
+-- | Imports a 'ByteString' to use as private key.
+-- The 'ByteString' must be exactly 32 bytes long for this to work.
+--
+-- Though minor changes may be made to create a valid key this property is guaranteed:
+-- prop> (\x -> importPrivate x >>= (importPrivate . exportPrivate)) = importPrivate
+importPrivate :: ByteString -> Maybe PrivateKey
+importPrivate bstr
+  | BS.length bstr /= 32 = Nothing
+  | otherwise  = 
+    let Just (b0, b1_31)  = BS.uncons bstr
+        Just (b1_30, b31) = BS.unsnoc b1_31
+        b0'               = b0   .&. 248
+        b31'              = b31  .&. 127
+        b31''             = b31' .|. 64
+        bytes             = (b0' `BS.cons` b1_30) `BS.snoc` b31''
+    in Just $ Priv bytes
+
+-- | Export a private key to a 'ByteString'
+exportPrivate :: PrivateKey -> ByteString
+exportPrivate (Priv bstr) = bstr
+
 
 -- |Randomly generate a Curve25519 public key.
 generatePublic :: PrivateKey -> PublicKey
 generatePublic (Priv priv) = Pub (curve25519 priv basePoint)
 
--- |Import a public key from a ByteString. The ByteString must be exactly
--- 32 bytes long for this to work.
+-- |Import a public key from a 'ByteString'.
+-- The 'ByteString' must be exactly 32 bytes long for this to work.
 importPublic :: ByteString -> Maybe PublicKey
 importPublic bstr | BS.length bstr == 32 = Just (Pub bstr)
                   | otherwise            = Nothing
 
--- |Export a public key to a ByteString.
+-- |Export a public key to a 'ByteString'.
 exportPublic :: PublicKey -> ByteString
 exportPublic (Pub bstr) = bstr
 
@@ -92,10 +110,9 @@ basePoint :: ByteString
 basePoint = BS.cons 9 (BS.replicate 31 0)
 
 buildNumber :: ByteString -> Integer
-buildNumber bstr = run 0 (BS.unpack bstr)
+buildNumber bstr = foldl' run 0 (BS.unpack bstr)
  where
-  run acc []     = acc
-  run acc (x:xs) = run ((acc * 256) + fromIntegral x) xs
+  run acc x = (acc `shiftL` 8) + fromIntegral x
 
 foreign import ccall unsafe
   curve25519_donna :: Ptr Word8 -> Ptr CChar -> Ptr CChar -> IO ()
